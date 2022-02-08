81bdf4140631f2c3e3d17c5d548ddb9721084931
jsj <jsj@qq.com>
2022-02-08 20:37:28 +0800

    first commit

---
+++ src/main/java/com/systop/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.systop.git;
+
+/**
+ *
+ * @author Aben66677
+ * @date 2022/2/8
+ */
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
