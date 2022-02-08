57df868df0fd9a98996a9416ec139dc75623caa3
jsj <jsj@qq.com>
2022-02-08 20:50:14 +0800

    second commit

--- src/main/java/com/systop/git/gitTest.java
+++ src/main/java/com/systop/git/gitTest.java
@@ -9,5 +9,6 @@ package com.systop.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
